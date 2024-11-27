100d89ba8b577fc795d0d07de99d991a434fa3f4
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-11-27 18:07:19 +0300

    Add headers list refactor server to byte input set limit

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -11,8 +11,9 @@ public class Request {
     private final String method;
     private final String path;
     private List<NameValuePair> queryParams;
+    private final List<NameValuePair> headers;
 
-    public Request(String[] requestLine) {
+    public Request(String[] requestLine, String headersString) {
         this.method = requestLine[0];
         if (!requestLine[1].contains("?")) {
             this.path = requestLine[1];
@@ -20,8 +21,12 @@ public class Request {
             int queryStringBegin = requestLine[1].indexOf("?");
             this.path = requestLine[1].substring(0, queryStringBegin);
             this.queryParams = URLEncodedUtils.parse(requestLine[1].substring(queryStringBegin + 1), Charset.defaultCharset());
-            System.out.println(queryParams);
         }
+        this.headers = URLEncodedUtils.parse(headersString, Charset.defaultCharset());
+
+        System.out.println(getMethod());
+        System.out.println(queryParams);
+        System.out.println(headers);
     }
 
     public Optional<NameValuePair> getQueryParam(String name) {
@@ -29,7 +34,15 @@ public class Request {
                 .filter(o -> o.getName().equals(name))
                 .findAny();
     }
+    public Optional<NameValuePair> getHeader(String name) {
+        return headers.stream()
+                .filter(o -> o.getName().equals(name))
+                .findAny();
+    }
 
+    public List<NameValuePair> getHeaders() {
+        return headers;
+    }
     public List<NameValuePair> getQueryParams() {
         return queryParams;
     }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,11 +1,11 @@
 package ru.netology;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -39,6 +39,56 @@ public class Server extends Thread {
         out.flush();
     }
 
+    // Получение строки запроса.
+    private static String[] getRequestLine(byte[] buffer, int read, BufferedOutputStream out) throws IOException {
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+        }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+        }
+        return requestLine;
+    }
+
+    private static String getHeaders(byte[] buffer, int read, BufferedInputStream in, BufferedOutputStream out) throws IOException {
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = new String(headersBytes);
+        System.out.println(headers);
+        return headers;
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
     // Поток подключений.
     @Override
     public void run() {
@@ -54,18 +104,30 @@ public class Server extends Thread {
 
     //Обработка запроса.
     public Runnable newConnection(Socket socket) throws IOException {
-        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        var out = new BufferedOutputStream(socket.getOutputStream());
+        final var in = new BufferedInputStream(socket.getInputStream());
+        final var out = new BufferedOutputStream(socket.getOutputStream());
         return new Runnable() {
             @Override
             public void run() {
                 try {
-                    String[] input = in.readLine().split(" ");
-                    if (input.length != 3) badRequest(out);
-                    Request request = new Request(input);
+                    final var limit = 4096;
+
+                    in.mark(limit);
+                    final var buffer = new byte[limit];
+                    final var read = in.read(buffer);
+
+                    Request request = new Request(getRequestLine(buffer, read, out), getHeaders(buffer, read, in, out));
 
                     if (!availableHandlers.containsKey(request.getMethod()) || !availableHandlers.get(request.getMethod()).containsKey(request.getPath())) {
-                        notFound(out);
+//                        notFound(out);
+                        out.write((
+                                "HTTP/1.1 200 OK\r\n" +
+                                        "Content-Length: 0\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                        out.flush();
+                        System.out.println();
                     }
                     availableHandlers.get(request.getMethod()).get(request.getPath()).handle(request, out);
 
